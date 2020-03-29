d81b40f213b8a6ce8a09e15bc1e5416151fbe64f
bpbista <bista.coder@gmail.com>
2020-03-29 11:55:15 -0400

    Create Outlets and Actions
    Connected all buttons with outlets and actions on PlaySoundsViewController

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -15,8 +15,8 @@ class PlaySoundsViewController: UIViewController {
     // MARK: Outlets
 
     @IBOutlet weak var snailButton: UIButton!
+       @IBOutlet weak var rabbitButton: UIButton!
     @IBOutlet weak var chipmunkButton: UIButton!
-    @IBOutlet weak var rabbitButton: UIButton!
     @IBOutlet weak var vaderButton: UIButton!
     @IBOutlet weak var echoButton: UIButton!
     @IBOutlet weak var reverbButton: UIButton!
